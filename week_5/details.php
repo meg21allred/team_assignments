5003bc8bf94d68fc138ff6475e692a1ade34f80b
meganallred <meg21allred@gmail.com>
2020-10-13 10:32:43 -0400

    addind more detainls

--- week_5/details.php
+++ week_5/details.php
@@ -7,5 +7,54 @@
 </head>
 <body>
     <h1>Scripture Content</h1>
+    <?php
+    
+    try
+{
+  $dbUrl = getenv('DATABASE_URL');
+
+  $dbOpts = parse_url($dbUrl);
+
+  $dbHost = $dbOpts["host"];
+  $dbPort = $dbOpts["port"];
+  $dbUser = $dbOpts["user"];
+  $dbPassword = $dbOpts["pass"];
+  $dbName = ltrim($dbOpts["path"],'/');
+
+  $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+
+  $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $ex)
+{
+  echo 'Error!: ' . $ex->getMessage();
+  die();
+}
+
+
+echo '  <form action="week05.php" method="get">
+<label for="book">Enter Book: </label>
+<input type="text" name="bookToSearch" id="book">
+<input type="submit" name="search" value="Find">
+
+</form>';
+
+$book = '';
+
+$book = $_GET['bookToSearch'];
+
+echo "<ul>";
+
+foreach ($db->query("SELECT * FROM scriptures") as $row)
+{
+  echo "<li>";
+  echo "<strong>" . $row['book'] . "</strong> " . $row['chapter'] . ":" . $row['verse'] . " - " . '"' . $row['content'] . '"';
+  echo "</li>";
+}
+
+echo "</ul>";
+
+    
+    ?>
 </body>
 </html>
