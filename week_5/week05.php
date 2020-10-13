e518976150eee37a07a76df2a20672874d5ac295
meganallred <meg21allred@gmail.com>
2020-10-13 08:55:37 -0400

    adding content

--- week_5/week05.php
+++ week_5/week05.php
@@ -7,5 +7,41 @@
 </head>
 <body>
     <h1>Scripture List</h1>
+    <?php
+
+try
+{
+  $dbUrl = getenv('DATABASE_URL');
+
+  $dbOpts = parse_url($dbUrl);
+
+  $dbHost = $dbOpts["host"];
+  $dbPort = $dbOpts["port"];
+  $dbUser = $dbOpts["user"];
+  $dbPassword = $dbOpts["pass"];
+  $dbName = ltrim($dbOpts["path"],'/');
+
+  $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+
+  $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $ex)
+{
+  echo 'Error!: ' . $ex->getMessage();
+  die();
+}
+
+echo "<ul>";
+
+foreach ($db->query('SELECT * FROM scriptures') as $row)
+{
+  echo "<li>";
+  echo $row['book'] . " " . $row['chapter'] . ":" . $row['verse'] . " " . $row['content'];
+  echo "</li>";
+}
+
+echo "</ul>";
+
+    ?>
 </body>
 </html>
